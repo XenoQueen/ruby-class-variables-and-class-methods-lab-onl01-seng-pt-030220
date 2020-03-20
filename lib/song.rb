91a184bec62616a8b210eecc316ed4f7c8678fe2
Maika Kailani <queenmab051@gmail.com>
2020-03-20 22:30:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,19 +28,19 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre]
+      if genre_count[genre] #if the genre is already in the hash
         genre_count[genre] += 1
-      else
+      else #if it's a new genre
         genre_count[genre] = 1
       end
     end
     genre_count
   end
-  
+
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]
+      if artist_count[artist] #if the artist is already in the hash
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
