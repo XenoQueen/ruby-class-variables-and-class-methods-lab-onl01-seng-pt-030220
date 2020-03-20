464db899ba9678d59ac4cef1e2298ee209968ba4
Maika Kailani <queenmab051@gmail.com>
2020-03-20 22:17:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,17 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    
-    if genre_count[genre]
-      genre_count[genre] += 1
-    else
-      genre_count[genre] = 1
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
     end
+    genre_count
   end
+  
+  
   
   def self.artist_count
     
